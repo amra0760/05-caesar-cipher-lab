6224dc76dd45ad49f2b4db348bea11b0bff0de84
Amr Almaz <amra0760@hstat.org>
2017-12-08 16:35:04 +0000

    way more effiecient way

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -22,19 +22,7 @@ end
 # puts caesar_encode(user_string,user_offset)
 
 def caesar_decode(string,offset)
-  alphabet = [*('a'..'z'), *('A'..'Z')] 
-  string.split("").map { |letter| 
-  string_old_index = alphabet.index(letter)
-    if alphabet[26..51].include?(letter)
-      string_new_index = (string_old_index - offset)%26
-      alphabet[string_new_index].upcase
-    elsif alphabet[0..25].include?(letter)
-      string_new_index = (string_old_index - offset)%26
-      alphabet[string_new_index]
-    else
-      letter
-    end
-  }.join
+  caesar_encode(string, offset *-1)
 end
 # puts "What would you like to encode?"
 # user_string = gets.chomp
